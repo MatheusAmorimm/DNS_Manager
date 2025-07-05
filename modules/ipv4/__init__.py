f8f55c4565b239bea7305490e54eec58dd3d500d
matheus amorim <mtxdevfrontend@gmail.com>
2025-07-05 12:50:52 -0300

    feat: add new features on ipv4

--- modules/ipv4/__init__.py
+++ modules/ipv4/__init__.py
@@ -1,4 +1,7 @@
 import ipaddress
+import subprocess
+from modules.log import log
+from time import sleep
 
 def ipv4_isvalid(ip):
     try:
@@ -6,3 +9,24 @@ def ipv4_isvalid(ip):
         return True
     except ipaddress.AddressValueError:
         return False
+    
+def disable_ipv4():
+    log("🔧 Desativando IPv6 em todas as interfaces via PowerShell...")
+    try:
+        subprocess.run('powershell "Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip -Confirm:$false"', shell=True, check=True)
+        log("✅ IPv6 desativado com sucesso em todas as interfaces.")
+        sleep(2)
+    except subprocess.CalledProcessError as e:
+        log(f"❌ Falha ao desativar IPv6. Erro: {e}")
+        sleep(2)
+
+
+def enable_ipv4():
+    log("🔧 Ativando IPv4 em todas as interfaces via PowerShell...")
+    try:
+        subprocess.run('powershell "Enable-NetAdapterBinding -Name * -ComponentID ms_tcpip -Confirm:$false"', shell=True, check=True)
+        log("✅ IPv4 ativado com sucesso em todas as interfaces.")
+        sleep(2)
+    except subprocess.CalledProcessError as e:
+        log(f"❌ Falha ao ativar IPv4. Erro: {e}")
+        sleep(2)
